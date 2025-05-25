12e90167466e20b9878eb814116b0802cc35c03d
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 22:53:09 +0200

    Update servers.ts

--- src/data/servers.ts
+++ src/data/servers.ts
@@ -19,7 +19,7 @@ export const discordServers: DiscordServer[] = [
     description: '🎨 Tu crées, testes et explores, mais tu as parfois l’impression de tourner en rond ? Tu n’es pas seul(e) : beaucoup de graphistes passionné(e)s se posent les mêmes questions, et c’est tout à fait normal. 🌟 C’est pourquoi on a lancé Le Repère des Graphistes, un serveur Discord pensé pour toi. Tu y trouveras des retours bienveillants, des ressources utiles, des défis stimulants et une vraie communauté de créatif(ve)s. Ici, pas de pression : juste du partage, de l’entraide et de la motivation pour avancer ensemble.',
     category: 'graphiste',
     inviteLink: 'https://discord.gg/EjKBs4kf6k',
-    iconUrl: 'https://media.discordapp.net/attachments/1372686113826934855/1375909100017160402/ReloadFrance.png?ex=683366b4&is=68321534&hm=d7ada5e5d94e8ccd693d3eff30d21c4e0e9a7e71f8be12b85f1b47865727b594&=&format=webp&quality=lossless',
+    iconUrl: 'https://media.discordapp.net/attachments/1376254397952299139/1376271382979154141/Sans_titre_89_20250306070915.png?ex=6834b81b&is=6833669b&hm=826d935d716686044299cf85455cfc66ed16e1a728e83b50feda92b2591c7c62&=&format=webp&quality=lossless&width=856&height=856',
     memberCount: 341,
     boostLevel: 22,
     promoted: true,
