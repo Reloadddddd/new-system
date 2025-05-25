e3ea4be940c287bacd0b0d58ffe248f5281758dc
Reloadddddd <reload.contactme@gmail.com>
2025-05-26 00:31:20 +0200

    Update servers.ts

--- src/data/servers.ts
+++ src/data/servers.ts
@@ -25,6 +25,18 @@ export const discordServers: DiscordServer[] = [
     promoted: true,
     promotionLevel: 'premium'
   },
+      {
+    id: '1',
+    name: 'Le Repère des graphistes',
+    description: '💼 NOS SERVICES : Développement de bots Discord sur mesure (RP, modération, économie…), Création de serveurs clés en main (RP, boutique, communautaire), Graphismes professionnels : logos, bannières, identités visuelles, Accompagnement sur vos projets Discord',
+    category: 'developper',
+    inviteLink: 'https://discord.gg/jXBUwUeXmx',
+    iconUrl: 'https://images-ext-1.discordapp.net/external/Mot4hkgT29bkInCppOtpp2bp2vIadMfkziuusgQeoQM/%3Fsize%3D256/https/cdn.discordapp.com/icons/1374611561154613328/1652265c810e2f79ddcc64dec2da0fb0.png?format=webp&quality=lossless',
+    memberCount: 15,
+    boostLevel: 1,
+    promoted: true,
+    promotionLevel: 'premium'
+  },
 ];
 
 export const promotedServers: DiscordServer[] = [
